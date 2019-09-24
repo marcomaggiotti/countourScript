6515586242158a858b54eb629f20483a251447e0
Marco Maggiotti <ing.marco.maggiotti@hotmail.it>
2019-09-24 18:27:48 +0200

    add maskAnalyser, percentage of white

---
+++ masksPercentageAnalyser.py
@@ -0,0 +1,41 @@
+import cv2
+import numpy as np
+import os
+import matplotlib.pyplot as plt
+import pandas as pd
+
+
+entries = os.listdir('/home/marco/Documents/Lab/deepLearning/MaskLou/')
+total_of_pixels = 88363080
+
+for entry in entries:
+    print(entry)
+
+    print("Immune" in entry)
+    if "Immune" in entry :
+    mask = cv2.imread('/home/marco/Documents/Lab/deepLearning/MaskLou/'+ entry,
+                     cv2.IMREAD_GRAYSCALE)
+
+    n_white_pix = np.sum(mask == 255)
+    print('Number of white pixels:', n_white_pix)
+
+    n_black_pix = np.sum(mask == 0)
+    print('Number of black pixels:', n_black_pix)
+
+    total_of_Pixels =  n_white_pix + n_black_pix
+    percentage = n_white_pix / ( total_of_pixels / 100 )
+
+    print(" Percentage number of Pixels : ", percentage )
+
+    ret, thresh = cv2.threshold(mask, 120, 255, cv2.THRESH_BINARY)
+    contours = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)[-2]
+
+    #print(contours)
+    dataframes = []
+    print(" Number of annotated Regions : ", len(contours))
+
+    for contour_id, contour in enumerate(contours):
+        #print(contour)
+        current_dataframe = pd.DataFrame(index=range(1,10))
+        #current_dataframe['contour'] = contour
+        dataframes.append(current_dataframe)
--- whiteCount.py
+++ whiteCount.py
@@ -6,9 +6,9 @@ import pandas as pd
 
 
 entries = os.listdir('/home/marco/Documents/Lab/deepLearning/MaskLou/')
-
-for entry in entries[0:1]:
-    #print(entry)
+dataframes = []
+for entry in entries[0:4]:
+    print(entry)
     mask = cv2.imread('/home/marco/Documents/Lab/deepLearning/MaskLou/'+ entry,
                      cv2.IMREAD_GRAYSCALE)
     n_white_pix = np.sum(mask == 255)
@@ -20,23 +20,21 @@ for entry in entries[0:1]:
     ret, thresh = cv2.threshold(mask, 120, 255, cv2.THRESH_BINARY)
     contours = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)[-2]
     #print(contours)
-    dataframes = []
 
-    for contour_id, contour in enumerate(contours):
-        for a in contour:
-            print(contour)
-            current_dataframe = pd.DataFrame(np.array(a), columns=['row', 'column'])
-            current_dataframe['contour'] = contour_id
+
+    for contour in contours:
+        for a,b in enumerate(contour):
+            #print(contour)
+            current_dataframe = pd.DataFrame(b, columns=['row', 'column'])
+            current_dataframe['contour'] = a
+            current_dataframe['filename'] = entry
+            current_dataframe['size'] = len(contour)
+            current_dataframe['White Number'] = n_white_pix
             dataframes.append(current_dataframe)
 
     contours_data = pd.concat(dataframes)
-    contours_data.to_excel('filename.xlsx', sheet_name='Sheet1')
-
-    df = pd.DataFrame({'Data': contours})
-    writer = pd.ExcelWriter('pandas_simple.xlsx', engine='xlsxwriter')
-    df.to_excel(writer, sheet_name='Sheet1')
-    writer.save()
-    #cv2.imshow("Mask", mask)
-    for contour in contours:
-        print(contour)
-        cv2.drawContours(mask, contour, -1, (0, 255, 0), 3)
+contours_data.to_excel('filename.xlsx', sheet_name='Sheet1')
+#cv2.imshow("Mask", mask)
+for contour in contours:
+    #print(contour)
+    cv2.drawContours(mask, contour, -1, (0, 255, 0), 3)
